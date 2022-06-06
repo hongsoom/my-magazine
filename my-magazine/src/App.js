964167aa93d6c425c00b0d7e4b1233bef2feba24
hongsoom <hongsoom@kakao.com>
2022-06-07 08:49:29 +0900

    Route

--- my-magazine/src/App.js
+++ my-magazine/src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Route, Routes } from "react-router-dom";
+import Header from "../src/component/Header";
+import Main from "./page/Main";
+import Add from "./page/Add";
+import Detail from "./page/Detail";
+import Join from "./page/Join";
+import Login from "./page/Login";
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/Add" element={<Add />} />
+        <Route path="/Join" element={<Join />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Detail/:index" element={<Detail />} />
+      </Routes>
     </div>
   );
 }
