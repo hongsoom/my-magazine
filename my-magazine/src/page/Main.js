3d18102ba54c02decc5de3549497cf00942642aa
xira94 <105337483+xira94@users.noreply.github.com>
2022-06-09 16:28:58 +0900

    Update Main.js

--- my-magazine/src/page/Main.js
+++ my-magazine/src/page/Main.js
@@ -1,46 +1,35 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Post from "../component/post";
-import styled from 'styled-components';
-import { FaPlus } from "react-icons/fa";
+import styled from "styled-components";
+import { FaPlusCircle } from "react-icons/fa";
 
 const Main = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const is_login = useSelector((state) => state.user.is_login);
 
-    const is_login = useSelector((state) => state.user.is_login);
-    
-    return (
-        <Content>
-            <Post/>
-            {is_login ? <Button onClick={() => navigate('/Add')}><FaPlus size='25'/></Button> : null }
-        </Content>
-    )
-}
-
-const Content = styled.div`
-    display : flex;
-    flex-direction : column;
-    align-items: center;
-    margin-top: 100px;
-    position:relative; 
-    margin-bottom:45px; 
-`;
+  return (
+    <>
+      <Post />
+      {is_login ? (
+        <Button onClick={() => navigate("/Add")}>
+          <FaPlusCircle size="40" color="rgb(222, 159, 228)" />
+        </Button>
+      ) : null}
+    </>
+  );
+};
 
 const Button = styled.div`
-    position: fixed;
-    bottom: 0;
-    right: 0;   
-    cursor : pointer;
-    background-color: #C0C0C0;
-    dlsplay : block;
-    width : 50px;
-    height : 50px;
-    border-radius: 50%;
-    margin : 50px;
-    align-items: center;
-
-`;  
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  cursor: pointer;
+  width: 50px;
+  height: 50px;
+  margin: 50px;
+`;
 
 export default Main;
